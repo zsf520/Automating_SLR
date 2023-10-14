a55dc5d37856e1a3db4fd888dfd31b8921d43514
zsf520 <yunhaizhang687@gmail.com>
2023-10-14 16:57:58 +0200

    updates

--- Automating_SLR.py
+++ Automating_SLR.py
@@ -19,3 +19,5 @@ if __name__ == "__main__":
 
     # Call the processing function, remove duplicates and generate CSV, and apply filtering conditions
     bib_to_csv(bib_files, output_csv)
+
+    input("\nPress 'ENTER' to exit...")
